f46fba32f3c1a853dc5c817fde20e92cf747f8c0
Sergio Callegari <sergio.callegari@unibo.it>
2015-06-10 12:02:52 +0200

    Provide correct permissions in package

--- package.sh
+++ package.sh
@@ -6,6 +6,8 @@ PKGCNT=("draftwatermark.dtx" "draftwatermark.ins" "README")
 
 mkdir -p buildpkg/"$PKGNAME"_"$PKGVERS"
 cp "${PKGCNT[@]}" buildpkg/"$PKGNAME"_"$PKGVERS"
+chmod a+rx buildpkg/"$PKGNAME"_"$PKGVERS"
+chmod a+r buildpkg/"$PKGNAME"_"$PKGVERS"/*
 cd buildpkg
 zip -r -9 "$PKGNAME"_"$PKGVERS".zip "$PKGNAME"_"$PKGVERS"
 rm -fr "$PKGNAME"_"$PKGVERS"
